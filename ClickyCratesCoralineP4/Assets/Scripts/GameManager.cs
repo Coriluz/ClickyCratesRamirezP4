18bff3b1384e2b31082da5144faf629b2ff8010a
Coriluz <760351@student.dusd.net>
2025-05-22 11:44:09 -0700

    I'm on step four
    I finished everything else I wrote down isgameactive = true but nothing else. I stopped at 4:29 video wise.

--- ClickyCratesCoralineP4/Assets/Scripts/GameManager.cs
+++ ClickyCratesCoralineP4/Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
     // Start is called before the first frame update
@@ -15,7 +17,8 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
-       
+
+        isGameActive = true;
     }
 
     // Update is called once per frame
@@ -38,5 +41,10 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
 }
--- ClickyCratesCoralineP4/Assets/Scripts/Target.cs
+++ ClickyCratesCoralineP4/Assets/Scripts/Target.cs
@@ -11,6 +11,10 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+
+    public ParticleSystem explosionParticle;
+
+    public int pointValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,11 +35,17 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
     Vector3 RandomForce()
     {
