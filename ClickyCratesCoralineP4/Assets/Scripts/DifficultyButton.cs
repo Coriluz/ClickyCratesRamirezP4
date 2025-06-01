1c8aedd3dc0f57d9648bb00ee120972cac4125b3
Coriluz <760351@student.dusd.net>
2025-05-31 21:39:24 -0700

    Fixed Complier error
    I finished 5.4, I need to do the bonuses now.

--- ClickyCratesCoralineP4/Assets/Scripts/DifficultyButton.cs
+++ ClickyCratesCoralineP4/Assets/Scripts/DifficultyButton.cs
@@ -1,26 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI
+using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
-    private Button button;
-    // Start is called before the first frame update
-    void Start()
-    {
-       button = GetComponent<Button>();
-       button.onClick.AddListener(SetDifficulty);
-    }
+	private Button button;
+	private GameManager gameManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public int difficulty;
 
-    void SetDifficulty()
-    {
-        Debug.Log(gameObject.name + "was clicked");
-    }
+	//Start is called before the first frame update
+	void Start()
+	{
+		button = GetComponent<Button>();
+		gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+		button.onClick.AddListener(SetDifficulty);
+	}
+
+	//Update is called once per face
+	void Update()
+	{
+
+	}
+
+	void SetDifficulty()
+	{
+		Debug.Log(button.gameObject.name + " was clicked");
+		gameManager.StartGame(difficulty);
+	}
 }
--- ClickyCratesCoralineP4/Assets/Scripts/GameManager.cs
+++ ClickyCratesCoralineP4/Assets/Scripts/GameManager.cs
@@ -13,13 +13,10 @@ public class GameManager : MonoBehaviour
     public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
+    public GameObject titleScreen;
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
 
 
     }
@@ -55,5 +52,17 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
     }
 }
